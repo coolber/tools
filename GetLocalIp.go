a9011b460d5074255c41aeb337a9b05a2632d06c
coolber <coolcole@139.com>
2021-07-15 11:32:47 +0800

    return err

--- GetLocalIp.go
+++ GetLocalIp.go
@@ -6,13 +6,14 @@ import (
 	"strings"
 )
 
-func GetLocalIp() (ip string) {
+func GetLocalIp() (ip string, err error) {
 	conn, err := net.Dial("udp", "8.8.8.8:53")
 	defer conn.Close()
 	if err != nil {
 		log.Printf("Get ip error: %v", err)
-		return ""
+		ip = ""
 	}
 	ip = strings.Split(conn.LocalAddr().String(), ":")[0]
-	return ip
+
+	return ip, err
 }
